d37491bbb699ca4b90cd2fd5b2dd13dff073720b
bo-oz <boristoet80@gmail.com>
2018-08-03 13:50:25 +0200

    upgrade

--- lib/materialize_components/base.rb
+++ lib/materialize_components/base.rb
@@ -1,7 +1,7 @@
 module MaterializeComponents
 
   class Base
-    #include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::TagHelper
 
     attr_accessor :css_class
 
@@ -74,7 +74,7 @@ module MaterializeComponents
 
       def html_attributes
         @attr = (!@attr.is_a?(Hash) ? {} : @attr)
-        @attr.merge(style: @style, class: @css_class.join(' '))
+        @attr.merge(style: @style, class: @css_class.join(' ').strip)
       end
 
       def output
